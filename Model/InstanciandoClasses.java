db7d397dfd3d868aa12658036d9731ef2e7113c2
Amanda Campos <amanda.acampos2000@gmail.com>
2023-06-04 14:24:54 -0300

    corrigi package

--- Model/InstanciandoClasses.java
+++ Model/InstanciandoClasses.java
@@ -1,20 +1,10 @@
-package Model;
 
 import java.util.ArrayList;
 
 public class InstanciandoClasses {
     public static void main(String[] args) {
+    
        
-        //Administrador
-        Administrador Maria = new Administrador("Maria", "ABC123", 2710);
-        System.out.println(Maria);
-        System.out.println();
-        
-        //Cliente
-        Cliente Joao = new Cliente("Joâo", "12345678", "11111111111");
-        System.out.println(Joao);
-        System.out.println();
-
         //Cosmetico
         Cosmetico laRoche = new Cosmetico("La Roche-Posay Effaclar Concentrado", "30/09/2024", "La Roche-Posay", 79.90, "Gel de limpeza facial");
         System.out.println(laRoche);
@@ -33,10 +23,5 @@ public class InstanciandoClasses {
         System.out.println(farm);
         System.out.println();
 
-
-        //Venda
-        Venda venda1 = new Venda(01, Joao, paracetamol, 2);
-        System.out.println(venda1);
-        System.out.println();
     }
 }
